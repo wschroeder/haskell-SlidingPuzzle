f5ef1d8433493d9acd8fcddb52bad7c6ef5e6bb3
William Schroeder <wschroeder@gmail.com>
2016-02-28 16:08:08 -0600

    Forgot to add Screen.hs!

---
+++ src/Screen.hs
@@ -0,0 +1,12 @@
+module Screen (
+    clearScreen
+) where
+
+import qualified System.Console.ANSI as ANSI
+
+-- | Clears the screen and places the cursor at the top-left corner.
+clearScreen :: IO ()
+clearScreen = do
+    ANSI.clearScreen
+    ANSI.setCursorPosition 0 0
+
